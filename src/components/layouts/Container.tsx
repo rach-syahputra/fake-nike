bb42bbcd6491f377558cac550bf0dee67df7c142
rach-syahputra <rach.syahputra@gmail.com>
2024-12-24 00:11:26 +0800

    add Container component

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -8,10 +8,7 @@ type ContainerProps = {
 export default function Container({ children, className }: ContainerProps) {
   return (
     <div
-      className={cn(
-        'mx-auto w-full px-4 md:px-6 lg:max-w-screen-xl lg:px-8',
-        className
-      )}
+      className={cn('mx-auto w-full px-6 md:px-8 lg:px-10 xl:px-12', className)}
     >
       {children}
     </div>
