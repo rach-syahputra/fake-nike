163fee59851053b1691e87bb8119af5b55759e06
rach-syahputra <rach.syahputra@gmail.com>
2024-12-25 05:14:22 +0800

    implement responsive SearchInput on Navbar

--- src/components/navbar/SearchInput.tsx
+++ src/components/navbar/SearchInput.tsx
@@ -8,17 +8,24 @@ export default function SearchInput() {
   const { setOnSearch } = useSearch()
 
   return (
-    <form className='hidden w-36 items-center justify-center gap-2 rounded-full bg-gray-100 px-3 py-1 lg:flex'>
-      <Icon icon={faMagnifyingGlass} className='h-5 w-5 text-gray-900' />
-      <label htmlFor='search' className='sr-only'>
-        Search
-      </label>
-      <input
-        type='text'
-        placeholder='Search'
-        onClick={() => setOnSearch(true)}
-        className='h-full w-full bg-gray-100 font-medium focus-within:outline-none'
-      />
-    </form>
+    <div onClick={() => setOnSearch(true)}>
+      <div className='hidden w-36 items-center justify-center gap-2 rounded-full bg-gray-100 px-3 py-1 md:flex'>
+        <Icon icon={faMagnifyingGlass} className='h-5 w-5 text-gray-900' />
+        <label htmlFor='search' className='sr-only'>
+          Search
+        </label>
+        <input
+          type='text'
+          placeholder='Search'
+          className='h-full w-full bg-gray-100 font-medium focus-within:outline-none'
+        />
+      </div>
+      <div className='flex h-8 w-8 items-center justify-center md:hidden'>
+        <Icon
+          icon={faMagnifyingGlass}
+          className='h-[18px] w-[18px] text-gray-900'
+        />
+      </div>
+    </div>
   )
 }
