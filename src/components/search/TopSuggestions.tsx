aecfcff7089dc2e5a16c536fbcf0aab27d329cb6
rach-syahputra <rach.syahputra@gmail.com>
2024-12-26 11:03:54 +0800

    adjust TopSuggestions component styles

--- src/components/search/TopSuggestions.tsx
+++ src/components/search/TopSuggestions.tsx
@@ -9,14 +9,17 @@ export default function TopSuggestions({
   topSuggestions
 }: TopSuggestionsProps) {
   return (
-    <div className='col-span-2 flex h-full flex-col items-start gap-4 px-3'>
+    <div className='col-span-2 flex h-full flex-col items-start gap-4 place-self-start px-3'>
       <p className='font-[family-name:var(--font-helvetica-now-text)] text-sm text-gray-500'>
         Top Suggestions
       </p>
-      <ul className='flex flex-col gap-3'>
+      <ul className='flex h-full flex-col'>
         {topSuggestions.map((topSuggestion, index) => (
-          <li key={index}>
-            <Link href={`/products/${topSuggestion.slug}`}>
+          <li key={index} className='rounded-lg py-2'>
+            <Link
+              href={`/products/${topSuggestion.slug}`}
+              className='line-clamp-2 py-2 font-[family-name:var(--font-helvetica-now-text-medium)] text-lg hover:text-gray-500'
+            >
               {topSuggestion.name.toLocaleLowerCase()}
             </Link>
           </li>
