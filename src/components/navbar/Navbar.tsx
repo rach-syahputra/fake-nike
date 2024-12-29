3a8d0903e43ad2aa00604cb7933f46d45330e4fa
rach-syahputra <rach.syahputra@gmail.com>
2024-12-29 11:52:27 +0800

    adjust Navbar and ModalContainer z-index

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ import Logo from '../elements/Logo'
 
 export default function Navbar() {
   return (
-    <nav className='sticky top-0 z-10 flex h-[60px] w-full items-center justify-center border bg-white'>
+    <nav className='sticky top-0 z-20 flex h-[60px] w-full items-center justify-center border bg-white'>
       <Container className='flex items-center justify-between gap-4 md:grid md:grid-cols-3'>
         <Logo className='h-[20.57px] w-12 md:h-[27.43px] md:w-16' />
 
