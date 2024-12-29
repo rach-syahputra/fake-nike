6cb23aaa6b5ff19dc0f9f4ca16c9d03cc6c517ec
rach-syahputra <rach.syahputra@gmail.com>
2024-12-29 11:55:21 +0800

    implement sync SelectedCount on SizeFilter components

--- src/app/(main)/search/_components/filter/SizeFilter.tsx
+++ src/app/(main)/search/_components/filter/SizeFilter.tsx
@@ -1,17 +1,23 @@
 'use client'
 
-import { useState } from 'react'
-import { sizes } from '@/lib/constants/products'
+import { useEffect, useState } from 'react'
+import { sizes as sizeData } from '@/lib/constants/products'
+import { useFilterContext } from '@/context/FilterContext'
 import FilterCard from './FilterCard'
 import SizeItem from './SizeItem'
 
 export default function SizeFilter() {
+  const { sizes } = useFilterContext()
   const [selectedCount, setSelectedCount] = useState<number>(0)
+
+  useEffect(() => {
+    setSelectedCount(sizes.length)
+  }, [sizes])
 
   return (
     <FilterCard title='Size' selectedCount={selectedCount}>
       <div className='grid grid-cols-3 gap-x-3 gap-y-2'>
-        {sizes.map((size, index) => (
+        {sizeData.map((size, index) => (
           <SizeItem
             key={index}
             size={size}
--- src/app/(main)/search/_components/filter/mobile/SizeFilter.tsx
+++ src/app/(main)/search/_components/filter/mobile/SizeFilter.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { sizes as sizesData } from '@/lib/constants/products'
+import { sizes as sizeData } from '@/lib/constants/products'
 import { useFilterContext } from '@/context/FilterContext'
 import SizeItem from '../SizeItem'
 import MobileFilterCard from './MobileFilterCard'
@@ -17,7 +17,7 @@ export default function SizeFilter() {
   return (
     <MobileFilterCard title='Size' selectedCount={selectedCount}>
       <div className='flex flex-wrap items-center gap-2.5 gap-y-1.5'>
-        {sizesData.map((size, index) => (
+        {sizeData.map((size, index) => (
           <SizeItem
             key={index}
             size={size}
