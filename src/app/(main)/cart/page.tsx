e5f9c06b8710a13d61d21d0e53518408bdf0898d
rach-syahputra <rach.syahputra@gmail.com>
2024-12-31 07:37:53 +0800

    add Cart page

---
+++ src/app/(main)/cart/page.tsx
@@ -0,0 +1,14 @@
+import Container from '@/components/layouts/Container'
+import Bag from './_components/Bag'
+import Summary from './_components/Summary'
+
+export default function CartPage() {
+  return (
+    <Container className='max-w-screen-lg px-3 py-3 md:px-3 lg:px-0 xl:px-6'>
+      <div className='relative grid items-start gap-6 lg:grid-cols-12'>
+        <Bag className='lg:col-span-8' />
+        <Summary className='sticky top-[72px] lg:col-span-4' />
+      </div>
+    </Container>
+  )
+}
--- src/components/navbar/Cart.tsx
+++ src/components/navbar/Cart.tsx
@@ -1,20 +1,23 @@
 'use client'
 
-import { useCartContext } from '@/context/CartContext'
+import Link from 'next/link'
 import Image from 'next/image'
+import { useCartContext } from '@/context/CartContext'
 
 export default function Cart() {
   const { cart } = useCartContext()
 
   return (
     <div className='relative flex h-8 w-8 items-center justify-center rounded-full hover:bg-gray-200'>
-      <Image
-        src='/icons/market.png'
-        alt='cart icon'
-        width={40}
-        height={40}
-        className='h-auto w-[22px]'
-      />
+      <Link href='/cart' aria-label='Cart page'>
+        <Image
+          src='/icons/market.png'
+          alt='cart icon'
+          width={40}
+          height={40}
+          className='h-auto w-[22px]'
+        />
+      </Link>
       <p className='absolute bottom-1.5 select-none text-[10px] font-bold'>
         {cart.length}
       </p>
