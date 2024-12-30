52824f796f90d957cac99caabd10ea4e178c9ad9
rach-syahputra <rach.syahputra@gmail.com>
2024-12-30 16:44:55 +0800

    add LoadingSpinner component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { FilterProvider } from '@/context/FilterContext'
 import { SearchProvider } from '@/context/SearchContext'
 import Navbar from '@/components/navbar/Navbar'
 import SearchBar from '@/components/search/SearchBar'
+import LoadingSpinner from '@/components/elements/LoadingSpinner'
 
 export default function MainLayout({
   children
@@ -13,7 +14,13 @@ export default function MainLayout({
   return (
     <>
       <NavigationProvider>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <div className='flex min-h-screen w-screen items-center justify-center'>
+              <LoadingSpinner />
+            </div>
+          }
+        >
           <FilterProvider>
             <SearchProvider>
               <Navbar />
