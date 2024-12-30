d32852cd809f75d8f70b89319bc0e36dfb9591b1
rach-syahputra <rach.syahputra@gmail.com>
2024-12-30 14:52:04 +0800

    install Shadcnui components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,26 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
 export function capitalizeFirstLetter(string: string) {
   const trimmedString = string.trim()
   if (!trimmedString) return ''
   return trimmedString.charAt(0).toUpperCase() + trimmedString.slice(1)
 }
 
-export function formatToRupiah(amount: number): string {
-  return new Intl.NumberFormat('id-ID', {
+export function formatToRupiah(amount: number, replaceDots: string): string {
+  const formattedAmount = new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
     minimumFractionDigits: 0
   })
     .format(amount)
     .replace('Rp', 'Rp ')
-    .replaceAll('.', ',')
+
+  return replaceDots
+    ? formattedAmount.replaceAll('.', `${replaceDots}`)
+    : formattedAmount
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
