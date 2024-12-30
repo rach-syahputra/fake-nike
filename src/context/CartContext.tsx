fd34a4f208d15ffc24caeae7629d1c2f8c03b2e6
rach-syahputra <rach.syahputra@gmail.com>
2024-12-31 07:30:15 +0800

    add cartProducts state to CartContext

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -10,7 +10,14 @@ import {
 } from 'react'
 import { getLocalStorage, setLocalStorage } from '@/hooks/local-storage'
 import { cartKey } from '@/lib/constants/cart'
-import { IAddedProduct, ICart, ICartContextErrors } from '@/lib/types/types'
+import {
+  IAddedProduct,
+  ICart,
+  ICartContextErrors,
+  ICartProductCard,
+  IProductJson
+} from '@/lib/types/types'
+import { fetchCartProduct } from '@/lib/api/services'
 
 interface ICartContext {
   cart: ICart[]
@@ -21,12 +28,19 @@ interface ICartContext {
   setErrors: Dispatch<SetStateAction<ICartContextErrors | null>>
   AddedProduct: IAddedProduct | null
   setAddedProduct: Dispatch<SetStateAction<IAddedProduct | null>>
+  cartProducts: ICartProductCard[] | undefined
+  setCartProducts: Dispatch<SetStateAction<ICartProductCard[] | undefined>>
+  increaseCount: ({ id, size }: { id: string; size: number }) => void
+  decreaseCount: ({ id, size }: { id: string; size: number }) => void
 }
 
 const CartContext = createContext<ICartContext | undefined>(undefined)
 
 const CartProvider = ({ children }: { children: React.ReactNode }) => {
   const [cart, setCart] = useState<ICart[]>([])
+  const [cartProducts, setCartProducts] = useState<
+    ICartProductCard[] | undefined
+  >(undefined)
   const [selectedSize, setSelectedSize] = useState<string>('')
   const [AddedProduct, setAddedProduct] = useState<IAddedProduct | null>(null)
   const [errors, setErrors] = useState<ICartContextErrors | null>(null)
@@ -43,10 +57,10 @@ const CartProvider = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     setLocalStorage(cartKey, cart)
+    updateCartProducts()
   }, [cart])
 
   const addToCart = ({ id, size }: { id: string; size: number }) => {
-    console.log('addToCart is called')
     if (size) {
       setErrors({ size: '' })
 
@@ -75,6 +89,67 @@ const CartProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }
 
+  const updateCartProducts = async () => {
+    const cartProductIds = cart?.map((data) => data.id)
+
+    const data: IProductJson[] = await fetchCartProduct(cartProductIds)
+
+    const mergedData: ICartProductCard[] | undefined = cart?.map((cart) => {
+      const product = data.find((item) => item.id === cart.id)
+
+      return {
+        size: cart.size,
+        count: cart.count,
+        name: product?.name || '',
+        id: product?.id || '',
+        category: product?.category || '',
+        price: product?.price || 0,
+        imageUrl: product?.imageUrls[0] || ''
+      }
+    })
+
+    setCartProducts(mergedData)
+  }
+
+  const increaseCount = ({ id, size }: { id: string; size: number }) => {
+    setCart((prevState) => {
+      const existingIndex = prevState.findIndex(
+        (item) => item.id === id && item.size === size
+      )
+
+      if (existingIndex !== -1) {
+        const updatedCart = [...prevState]
+        updatedCart[existingIndex].count += 1
+
+        return updatedCart
+      }
+
+      return [...prevState]
+    })
+  }
+
+  const decreaseCount = ({ id, size }: { id: string; size: number }) => {
+    setCart((prevState) => {
+      const existingIndex = prevState.findIndex(
+        (item) => item.id === id && item.size === size
+      )
+
+      if (existingIndex !== -1) {
+        const updatedCart = [...prevState]
+
+        updatedCart[existingIndex].count -= 1
+
+        if (updatedCart[existingIndex].count === 0) {
+          updatedCart.splice(existingIndex, 1)
+        }
+
+        return updatedCart
+      }
+
+      return [...prevState]
+    })
+  }
+
   return (
     <CartContext.Provider
       value={{
@@ -85,7 +160,11 @@ const CartProvider = ({ children }: { children: React.ReactNode }) => {
         errors,
         setErrors,
         AddedProduct,
-        setAddedProduct
+        setAddedProduct,
+        cartProducts,
+        setCartProducts,
+        increaseCount,
+        decreaseCount
       }}
     >
       {children}
