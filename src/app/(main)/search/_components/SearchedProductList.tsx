0dcd2c545a9cfd7a9b2a6c589c2f9ec391106a99
rach-syahputra <rach.syahputra@gmail.com>
2024-12-31 08:21:03 +0800

    implement Load More Products on Search Page

--- src/app/(main)/search/_components/SearchedProductList.tsx
+++ src/app/(main)/search/_components/SearchedProductList.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { fetchFilteredProducts } from '@/lib/api/services'
 import { IProductJson } from '@/lib/types/types'
 import { useFilteredProductsContext } from '@/context/FilteredProductsContext'
@@ -8,34 +8,50 @@ import { useFilterContext } from '@/context/FilterContext'
 import SearchProductCard from './SearchedProductCard'
 import Container from '@/components/layouts/Container'
 import SearchedProductCardSkeleton from './loading/SearchedProductCardSkeleton'
+import Button from '@/components/elements/Button'
 
 export default function SearchedProductList() {
   const { searchQuery, sort, order, categories, sizes } = useFilterContext()
   const { products, setProducts } = useFilteredProductsContext()
-
-  useEffect(() => {
-    getFilteredProducts()
-  }, [searchQuery, sort, order, categories, sizes])
+  const [page, setPage] = useState<number>(1)
+  const productListContainerRef = useRef<HTMLDivElement | null>(null)
 
   const getFilteredProducts = async () => {
-    const data: IProductJson[] = await fetchFilteredProducts(searchQuery, {
+    const res: IProductJson[] = await fetchFilteredProducts(searchQuery, {
       categories: categories,
       order: order,
       sort: sort,
-      sizes: sizes
+      sizes: sizes,
+      limit: 9,
+      page: page
     })
 
-    setProducts(
-      data.map((item) => ({
-        ...item,
-        imageUrl: item.imageUrls[0]
-      }))
-    )
+    const data = res.map((item) => ({
+      ...item,
+      imageUrl: item.imageUrls[0]
+    }))
+
+    setProducts((prevState) => (page === 1 ? data : [...prevState, ...data]))
   }
 
+  useEffect(() => {
+    getFilteredProducts()
+  }, [page])
+
+  useEffect(() => {
+    if (page === 1) {
+      getFilteredProducts()
+    } else {
+      setPage(1)
+    }
+  }, [searchQuery, sort, order, categories, sizes])
+
   return (
-    <Container className='px-0 py-1 md:px-0 lg:px-12'>
-      <div className='grid grid-cols-2 gap-1.5 pb-8 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-6'>
+    <Container className='px-0 py-1 pb-6 md:px-0 lg:px-12'>
+      <div
+        ref={productListContainerRef}
+        className='grid grid-cols-2 gap-1.5 overflow-auto pb-8 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-6'
+      >
         {products && products.length > 0 ? (
           products.map((product, index) => (
             <SearchProductCard key={index} {...product} />
@@ -48,6 +64,11 @@ export default function SearchedProductList() {
             <SearchedProductCardSkeleton />
           </>
         )}
+      </div>
+      <div className='flex items-center justify-center'>
+        <Button onClick={() => setPage((prev) => prev + 1)} className=''>
+          Load more
+        </Button>
       </div>
     </Container>
   )
