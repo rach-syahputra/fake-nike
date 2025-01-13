470ca9d3856385b7a5afb6c163e4117e1ecac03b
rach-syahputra <rach.syahputra@gmail.com>
2025-01-13 16:08:02 +0800

    feat: add Navbar visibility toggle based on scroll direction

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { SessionProvider } from 'next-auth/react'
+
+import { cn } from '@/lib/utils'
 import { useCartContext } from '@/context/CartContext'
 import Container from '../layouts/Container'
 import MobileMenu from './MobileMenu'
@@ -13,9 +16,36 @@ import AddedProductToCart from './AddedProductToCart'
 
 export default function Navbar() {
   const { AddedProduct } = useCartContext()
+  const [show, setShow] = useState<boolean>(true)
+  const [lastScrollY, setLastScrollY] = useState<number>(0)
+
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) {
+      setShow(false)
+    } else {
+      setShow(true)
+    }
+
+    setLastScrollY(window.scrollY)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar)
+
+    return () => {
+      window.removeEventListener('scroll', controlNavbar)
+    }
+  }, [lastScrollY])
 
   return (
-    <nav className='sticky top-0 z-20 flex h-[60px] w-full items-center justify-center border bg-white'>
+    <nav
+      className={cn(
+        'invisible sticky top-0 z-20 flex h-[60px] w-full items-center justify-center bg-white',
+        {
+          visible: show
+        }
+      )}
+    >
       <Container className='flex items-center justify-between gap-4 md:grid md:grid-cols-3'>
         <Logo className='h-[20.57px] w-12 md:h-[27.43px] md:w-16' />
 
