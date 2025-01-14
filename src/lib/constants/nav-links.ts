71aead3e4daa3caf73515a4a6ad5d52955111521
rach-syahputra <rach.syahputra@gmail.com>
2025-01-14 14:20:57 +0800

    feat: add Navigation Menu constants and types

--- src/lib/constants/nav-links.ts
+++ src/lib/constants/nav-links.ts
@@ -3,7 +3,7 @@ type NavLinks = {
   label: string
 }
 
-export const navLinks: NavLinks[] = [
+export const NAVBAR_MENU: NavLinks[] = [
   {
     href: '/search?q=&category=men',
     label: 'Men'
@@ -11,5 +11,178 @@ export const navLinks: NavLinks[] = [
   {
     href: '/search?q=&category=women',
     label: 'Women'
+  }
+]
+
+export const MEN_SHOES_MENU: NavLinks[] = [
+  {
+    href: '#',
+    label: 'All Shoes'
+  },
+  {
+    href: '#',
+    label: 'Lifestyle'
+  },
+  {
+    href: '#',
+    label: 'Jordan'
+  },
+  {
+    href: '#',
+    label: 'Running'
+  },
+  {
+    href: '#',
+    label: 'Football'
+  },
+  {
+    href: '#',
+    label: 'Basketball'
+  },
+  {
+    href: '#',
+    label: 'Gym and Training'
+  },
+  {
+    href: '#',
+    label: 'Skateboarding'
+  },
+  {
+    href: '#',
+    label: 'Sandals and Slides'
+  },
+  {
+    href: '#',
+    label: 'Nike By You'
+  }
+]
+
+export const WOMEN_SHOES_MENU: NavLinks[] = [
+  {
+    href: '#',
+    label: 'All Shoes'
+  },
+  {
+    href: '#',
+    label: 'Lifestyle'
+  },
+  {
+    href: '#',
+    label: 'Jordan'
+  },
+  {
+    href: '#',
+    label: 'Running'
+  },
+  {
+    href: '#',
+    label: 'Gym and Training'
+  },
+  {
+    href: '#',
+    label: 'Football'
+  },
+  {
+    href: '#',
+    label: 'Basketball'
+  },
+  {
+    href: '#',
+    label: 'Sandals and Slides'
+  },
+  {
+    href: '#',
+    label: 'Nike By You'
+  }
+]
+
+export const MEN_CLOTHING_MENU: NavLinks[] = [
+  {
+    href: '#',
+    label: 'All Clothing'
+  },
+  {
+    href: '#',
+    label: 'Performance Essentials'
+  },
+  {
+    href: '#',
+    label: 'Tops and T-Shirts'
+  },
+  {
+    href: '#',
+    label: 'Shorts'
+  },
+  {
+    href: '#',
+    label: 'Pants and Leggings'
+  },
+
+  {
+    href: '#',
+    label: 'Hoodies and Sweatshirts'
+  },
+  {
+    href: '#',
+    label: 'Jackets and Gilets'
+  },
+  {
+    href: '#',
+    label: 'Jerseys and Kits'
+  },
+  {
+    href: '#',
+    label: 'Jordan'
+  }
+]
+
+export const WOMEN_CLOTHING_MENU: NavLinks[] = [
+  {
+    href: '#',
+    label: 'All Clothing'
+  },
+  {
+    href: '#',
+    label: 'Performance Essentials'
+  },
+  {
+    href: '#',
+    label: 'Tops and T-Shirts'
+  },
+  {
+    href: '#',
+    label: 'Sports Bras'
+  },
+  {
+    href: '#',
+    label: 'Pants and Leggings'
+  },
+  {
+    href: '#',
+    label: 'Shorts'
+  },
+  {
+    href: '#',
+    label: 'Hoodies and Sweatshirts'
+  },
+  {
+    href: '#',
+    label: 'Jackets and Gilets'
+  },
+  {
+    href: '#',
+    label: 'Skirts and Dresses'
+  },
+  {
+    href: '#',
+    label: 'Modest Wear'
+  },
+  {
+    href: '#',
+    label: 'Nike Maternity'
+  },
+  {
+    href: '#',
+    label: 'Plus Size'
   }
 ]
---
+++ src/lib/types/menus.ts
@@ -0,0 +1,1 @@
+export type NavbarMenuType = 'men' | 'women'
