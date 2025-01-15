89cdfb0ad06d0d87f34965a8f6f2243f6a4a800a
rach-syahputra <rach.syahputra@gmail.com>
2025-01-15 11:08:56 +0800

    style: adjust Sidebar and SearchHeader component styles

--- src/app/(main)/search/_components/filter/Sidebar.tsx
+++ src/app/(main)/search/_components/filter/Sidebar.tsx
@@ -1,19 +1,24 @@
 'use client'
 
 import { cn } from '@/lib/utils'
+import { useNavigation } from '@/context/NavigationContext'
 import { useFilterContext } from '@/context/FilterContext'
 import GenderFilter from './GenderFilter'
 import SizeFilter from './SizeFilter'
 
 export default function Sidebar() {
+  const { showNavbar } = useNavigation()
   const { onSidebar } = useFilterContext()
 
   return (
     <div
       className={cn(
-        'sticky top-[120px] hidden w-[260px] flex-col gap-2 pl-12',
+        'sticky top-[60px] hidden w-[260px] flex-col gap-2 pl-12',
         {
           'lg:flex': onSidebar
+        },
+        {
+          'top-[120px]': showNavbar
         }
       )}
     >
--- src/app/(main)/search/_components/header/SearchHeader.tsx
+++ src/app/(main)/search/_components/header/SearchHeader.tsx
@@ -1,10 +1,23 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { useNavigation } from '@/context/NavigationContext'
 import Container from '@/components/layouts/Container'
 import SearchHeaderOptions from './SearchHeaderOptions'
 import SearchResult from './SearchResult'
 
 export default function SearchHeader() {
+  const { showNavbar } = useNavigation()
+
   return (
-    <Container className='sticky top-[60px] z-10 flex h-[60px] items-center justify-between border-b bg-white px-5 md:px-5 lg:border-none lg:px-12'>
+    <Container
+      className={cn(
+        'sticky top-0 z-10 flex h-[60px] items-center justify-between border-b bg-white px-5 md:px-5 lg:border-none lg:px-12',
+        {
+          'top-[60px]': showNavbar
+        }
+      )}
+    >
       <SearchResult />
       <SearchHeaderOptions />
     </Container>
