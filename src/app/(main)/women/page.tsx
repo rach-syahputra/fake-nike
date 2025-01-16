ab3775c676c14e3f527ca78e8ff5f08709906a0e
rach-syahputra <rach.syahputra@gmail.com>
2025-01-16 10:42:01 +0800

    feat: add Women page

---
+++ src/app/(main)/women/page.tsx
@@ -0,0 +1,9 @@
+import TheNikeSwiftCollection from './_components/TheNikeSwiftCollection'
+
+export default function WoMenPage() {
+  return (
+    <div className='flex flex-col'>
+      <TheNikeSwiftCollection />
+    </div>
+  )
+}
