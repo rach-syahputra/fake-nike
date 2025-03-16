7b1ba73d2c1485938aad856442d5ad18a954bc0c
rach-syahputra <rach.syahputra@gmail.com>
2025-03-17 01:49:45 +0800

    feat(client): add Football page

---
+++ client/src/app/(main)/football/page.tsx
@@ -0,0 +1,9 @@
+import FootballHero from './_components/FootballHero'
+
+export default function FoottballPage() {
+  return (
+    <div className='flex flex-col'>
+      <FootballHero />
+    </div>
+  )
+}
