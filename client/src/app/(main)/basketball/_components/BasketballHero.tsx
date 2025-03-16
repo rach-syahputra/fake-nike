b920d3ff77fe7719dafdaaaa2237f8c9646dada3
rach-syahputra <rach.syahputra@gmail.com>
2025-03-17 01:49:34 +0800

    feat(client): add Basketball page

---
+++ client/src/app/(main)/basketball/_components/BasketballHero.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useFilterContext } from '@/context/FilterContext'
+import {
+  CTASection,
+  CTASectionButton,
+  CTASectionContent,
+  CTASectionDescription,
+  CTASectionHeader,
+  CTASectionMedia,
+  CTASectionTitle
+} from '@/components/sections/CTASection'
+
+export default function BasketballHero() {
+  const { updateParams } = useFilterContext()
+
+  const handleCTAClick = () => {
+    updateParams({
+      q: 'kobe',
+      categories: [6]
+    })
+  }
+
+  return (
+    <CTASection>
+      <CTASectionMedia>
+        <video
+          preload='auto'
+          autoPlay
+          loop
+          muted
+          className='aspect-[3_/_4] h-auto w-full object-cover lg:aspect-auto'
+        >
+          <source src='/video/Nike Just Do It  - Landing Page.mov' />
+          Your browser does not support the video tag.
+        </video>
+      </CTASectionMedia>
+      <CTASectionContent>
+        <CTASectionHeader>
+          <CTASectionTitle>HAVE A HARD YEAR</CTASectionTitle>
+          <CTASectionDescription>
+            This isn&rsquo;t the Year of Snake. This is the Year of the Mamba
+          </CTASectionDescription>
+        </CTASectionHeader>
+        <CTASectionButton onClick={() => handleCTAClick()}>
+          Shop
+        </CTASectionButton>
+      </CTASectionContent>
+    </CTASection>
+  )
+}
