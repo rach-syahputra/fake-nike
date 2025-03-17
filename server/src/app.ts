dcda00e80cdc732db9e896f4f91d0bb626792bd7
rach-syahputra <rach.syahputra@gmail.com>
2025-03-17 16:37:11 +0800

    feat(server): update cors configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -24,16 +24,6 @@ class App {
 
   private configure() {
     this.app.use(cors(corsOptions))
-    this.app.options('*', (req, res) => {
-      res.header('Access-Control-Allow-Origin', 'https://fakenikex.vercel.app/')
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, OPTIONS'
-      )
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-      res.header('Access-Control-Allow-Credentials', 'true')
-      res.sendStatus(204)
-    })
     this.app.use(express.json())
   }
 
